095ac8ca537f7622bf78d7bb67e40e011ee39acb
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2018-10-29 19:37:30 +0100

    Change multiple example from markdown to csv

--- examples/multiple/index.js
+++ examples/multiple/index.js
@@ -1,30 +1,31 @@
 var Build = require('../../')
 var cli = require('../../cli')
+var csv = require('csv-parse/lib/sync')
 var fg = require('fast-glob')
 var fs = require('fs')
-var marked = require('marked')
 var path = require('path')
 
 var build = Build.dest(path.join(__dirname, 'target'))
 
-build.add('*.html', async function (params) {
+build.add('*.json', async function (params) {
   await this.prune()
 
   var post = params.wildcards[0]
-  var files = await fg(path.join(__dirname, 'src', post + '.md'))
+  var files = await fg(path.join(__dirname, 'src', post + '.csv'))
 
-  var pages = files.map(file => {
+  var targets = files.map(file => {
     var content = fs.readFileSync(file, 'utf8')
-    var name = path.basename(file, '.md')
+    var data = csv(content, { columns: true })
+    var name = path.basename(file, '.csv')
 
     return this.write({
-      path: name + '.html',
-      contents: marked(content),
+      path: name + '.json',
+      contents: JSON.stringify(data, null, 2),
       enc: 'utf8'
     })
   })
 
-  return Promise.all(pages)
+  return Promise.all(targets)
 })
 
 cli(build)
